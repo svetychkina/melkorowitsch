f7b448b39089ba9b8d5e3e4ae13cd0f122fce765
svetychkina <svetych494@gmail.com>
2017-05-01 21:37:30 +0300

    Spinners
    Попытка связать два спиннера. Ни черта не выходит.

--- MyApplication/app/src/main/java/com/example/conscience/myapplication/RangeFormActivity.java
+++ MyApplication/app/src/main/java/com/example/conscience/myapplication/RangeFormActivity.java
@@ -4,6 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static com.example.conscience.myapplication.R.id.specSpinner;
+import static com.example.conscience.myapplication.R.id.progSpinner;
 
 public class RangeFormActivity extends AppCompatActivity {
 
@@ -11,10 +18,44 @@ public class RangeFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_range_form);
+
+        Spinner progSpinner = (Spinner) findViewById(R.id.progSpinner);
+        progSpinner.setOnItemSelectedListener(this);
+
+        Spinner specSpinner = (Spinner) findViewById(R.id.specSpinner);
+
     }
 
     public void onClickRate(View view) {
         Intent intent = new Intent(RangeFormActivity.this, RateActivity.class);
         startActivity(intent);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+        parent.getItemAtPosition(pos);
+        if (pos == 1){
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.bachelor, android.R.layout.simple_spinner_item);
+            specSpinner.setAdapter(adapter);
+        }
+        else if (pos == 2){
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.magistracy, android.R.layout.simple_spinner_item);
+            specSpinner.setAdapter(adapter);
+        }
+        else if (pos == 3){
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.postgraduate, android.R.layout.simple_spinner_item);
+            specSpinner.setAdapter(adapter);
+        }
+        else if (pos == 4){
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.extramural, android.R.layout.simple_spinner_item);
+            specSpinner.setAdapter(adapter);
+        }
+        else if (pos == 5){
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.foreign, android.R.layout.simple_spinner_item);
+            specSpinner.setAdapter(adapter);
+        }
+        else if (pos == 6){
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.training, android.R.layout.simple_spinner_item);
+            specSpinner.setAdapter(adapter);
+        }
     }
 }
