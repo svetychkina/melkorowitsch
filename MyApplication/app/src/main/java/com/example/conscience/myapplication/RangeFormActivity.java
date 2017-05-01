a78997572d912b1ad1a959136c520147448d2f3d
svetychkina <svetych494@gmail.com>
2017-05-02 02:27:47 +0300

    Spinners[2]
    Связала два спиннера. Работает. Наконец-то...

--- MyApplication/app/src/main/java/com/example/conscience/myapplication/RangeFormActivity.java
+++ MyApplication/app/src/main/java/com/example/conscience/myapplication/RangeFormActivity.java
@@ -9,8 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import static com.example.conscience.myapplication.R.id.specSpinner;
-import static com.example.conscience.myapplication.R.id.progSpinner;
+//import static com.example.conscience.myapplication.R.id.specSpinner;
+//import static com.example.conscience.myapplication.R.id.progSpinner;
 
 public class RangeFormActivity extends AppCompatActivity {
 
@@ -19,43 +19,45 @@ public class RangeFormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_range_form);
 
-        Spinner progSpinner = (Spinner) findViewById(R.id.progSpinner);
-        progSpinner.setOnItemSelectedListener(this);
+        final Spinner progSpinner = (Spinner)findViewById(R.id.progSpinner);
+        final Spinner specSpinner = (Spinner)findViewById(R.id.specSpinner);
 
-        Spinner specSpinner = (Spinner) findViewById(R.id.specSpinner);
+        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this, R.array.programs, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-    }
+        progSpinner.setAdapter(adapter);
+
+        progSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            public void onItemSelected(AdapterView<?> parent, View itemSelected, int pos, long id){
+                //String[] choose = getResources().getStringArray(R.array.programs);
+                //Toast toast = Toast.makeText(getApplicationContext(),"your choise is " + choose[pos], Toast.LENGTH_SHORT);
+                //toast.show();
+
+                parent.getItemAtPosition(pos);
+                if (pos == 0){
+                    ArrayAdapter<CharSequence> specAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.bachelor, android.R.layout.simple_spinner_item);
+                    specSpinner.setAdapter(specAdapter);
+                }
+                else if (pos == 1){
+                    ArrayAdapter<CharSequence> specAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.magistracy, android.R.layout.simple_spinner_item);
+                    specSpinner.setAdapter(specAdapter);
+                }
+                else if (pos == 2){
+                    ArrayAdapter<CharSequence> specAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.postgraduate, android.R.layout.simple_spinner_item);
+                    specSpinner.setAdapter(specAdapter);
+                }
+
+            }
+
+            public void onNothingSelected(AdapterView<?> parent){}
+            });
+        }
+
+
+
 
     public void onClickRate(View view) {
         Intent intent = new Intent(RangeFormActivity.this, RateActivity.class);
         startActivity(intent);
-    }
-
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
-        parent.getItemAtPosition(pos);
-        if (pos == 1){
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.bachelor, android.R.layout.simple_spinner_item);
-            specSpinner.setAdapter(adapter);
-        }
-        else if (pos == 2){
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.magistracy, android.R.layout.simple_spinner_item);
-            specSpinner.setAdapter(adapter);
-        }
-        else if (pos == 3){
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.postgraduate, android.R.layout.simple_spinner_item);
-            specSpinner.setAdapter(adapter);
-        }
-        else if (pos == 4){
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.extramural, android.R.layout.simple_spinner_item);
-            specSpinner.setAdapter(adapter);
-        }
-        else if (pos == 5){
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.foreign, android.R.layout.simple_spinner_item);
-            specSpinner.setAdapter(adapter);
-        }
-        else if (pos == 6){
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.training, android.R.layout.simple_spinner_item);
-            specSpinner.setAdapter(adapter);
-        }
     }
 }
