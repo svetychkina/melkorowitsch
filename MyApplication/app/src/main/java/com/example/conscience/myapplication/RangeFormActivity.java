1dd87ad4c0660afa91564d6f3d03ed945157937a
svetychkina <svetych494@gmail.com>
2017-05-08 20:20:45 +0300

    оптимизация
    минус один спиннер, плюс уведомления

--- MyApplication/app/src/main/java/com/example/conscience/myapplication/RangeFormActivity.java
+++ MyApplication/app/src/main/java/com/example/conscience/myapplication/RangeFormActivity.java
@@ -1,5 +1,13 @@
 package com.example.conscience.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,143 +18,69 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
 
-//import static com.example.conscience.myapplication.R.id.specSpinner;
-//import static com.example.conscience.myapplication.R.id.progSpinner;
 
-public class RangeFormActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+public class RangeFormActivity extends AppCompatActivity
+implements CompoundButton.OnCheckedChangeListener {
 
-    /*String[] specbach = getResources().getStringArray(R.array.bachelor);
-    String[] specmag = getResources().getStringArray(R.array.magistracy);
-    String[] specpostgrad = getResources().getStringArray(R.array.postgraduate);*/
+    private static final int NOTIFY_ID=101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_range_form);
 
-        final Spinner progSpinner = (Spinner)findViewById(R.id.progSpinner);
-        final Spinner specSpinner = (Spinner)findViewById(R.id.specSpinner);
-
-        //MyCustomAdapter adapter = new MyCustomAdapter(RangeFormActivity.this,R.layout.row, specbach);
-
-
         Switch notifSwitch = (Switch)findViewById(R.id.notificationSwitch);
         if (notifSwitch != null){
             notifSwitch.setOnCheckedChangeListener(this);
         }
 
+        final Spinner specSpinner = (Spinner)findViewById(R.id.specSpinner);
 
+        String[] specbach = getResources().getStringArray(R.array.bachelor);
 
-        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this, R.array.programs, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> specAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,specbach);
+        specAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        specSpinner.setAdapter(specAdapter);
 
-        progSpinner.setAdapter(adapter);
-
-        progSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-            public void onItemSelected(AdapterView<?> parent, View itemSelected, int pos, long id){
-                //String[] choose = getResources().getStringArray(R.array.programs);
-                //Toast toast = Toast.makeText(getApplicationContext(),"your choise is " + choose[pos], Toast.LENGTH_SHORT);
-                //toast.show();
-
-                parent.getItemAtPosition(pos);
-                if (pos == 0){
-                    ArrayAdapter<CharSequence> specAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.bachelor, android.R.layout.simple_spinner_item);
-                    specAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-<<<<<<< HEAD
-                    //ArrayAdapter<String> specAdapter = new ArrayAdapter<String>(RangeFormActivity.this,R.layout.row,R.id.selspec,specbach);
-=======
-                    //ArrayAdapter<String> specAdapter = new ArrayAdapter<String>(this, R.layout.row, R.id.selspec, specbach);
->>>>>>> master
-
-                    specSpinner.setAdapter(specAdapter);
-
-                    specSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-                        public void onItemSelected(AdapterView<?> parent, View itemSel, int selPos, long posId){
-                            String[] selected = getResources().getStringArray(R.array.bachelor);
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-                            Toast toast = Toast.makeText(getApplicationContext(), "Вы выбрали "+selected[selPos], Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-
-                        public void onNothingSelected(AdapterView<?> parent){}
-                    });
-                    specAdapter.notifyDataSetChanged();
-
-                }
-                else if (pos == 1){
-                    ArrayAdapter<CharSequence> specAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.magistracy, android.R.layout.simple_spinner_item);
-                    specAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    specSpinner.setAdapter(specAdapter);
-
-                    specSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-                        public void onItemSelected(AdapterView<?> parent, View itemSel, int selPos, long posId){
-                            String[] selected = getResources().getStringArray(R.array.magistracy);
-                            Toast toast = Toast.makeText(getApplicationContext(), "Вы выбрали "+selected[selPos], Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-
-                        public void onNothingSelected(AdapterView<?> parent){}
-                    });
-                    specAdapter.notifyDataSetChanged();
-
-
-                }
-                else if (pos == 2){
-                    ArrayAdapter<CharSequence> specAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.postgraduate, android.R.layout.simple_spinner_item);
-                    specAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    specSpinner.setAdapter(specAdapter);
-
-                    specSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-                        public void onItemSelected(AdapterView<?> parent, View itemSel, int selPos, long posId){
-                            String[] selected = getResources().getStringArray(R.array.postgraduate);
-                            Toast toast = Toast.makeText(getApplicationContext(), "Вы выбрали "+selected[selPos], Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-
-                        public void onNothingSelected(AdapterView<?> parent){}
-                    });
-                    specAdapter.notifyDataSetChanged();
-
-
-                }
-
-                                //Spinner spinner = (Spinner)findViewById(R.id.specSpinner);
-                //String selected = spinner.getSelectedItem().toString();
-                //Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT).show();
-
-            }
-
-
-
-            public void onNothingSelected(AdapterView<?> parent){}
-            });
-
-        /*specSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+        specSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             public void onItemSelected(AdapterView<?> parent, View itemSel, int selPos, long posId){
                 String[] selected = getResources().getStringArray(R.array.bachelor);
-                Toast toast = Toast.makeText(getApplicationContext(), "choise is "+selected[selPos], Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getApplicationContext(), "Вы выбрали "+selected[selPos], Toast.LENGTH_SHORT);
                 toast.show();
             }
 
             public void onNothingSelected(AdapterView<?> parent){}
-        });*/
+        });
 
 
-            }
-
-
-
-        //String specSelected = specSpinner.getSelectedItem().toString();
-        //outSpecTextView.setText(specSelected);
-
-
-
-    @Override
+    }
+@Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         Toast.makeText(this, "Уведомления " + (isChecked ? "включены" : "отключены"), Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onCheckNotifs(View view) {
+        Context context = getApplicationContext();
+
+        Intent notIntent = new Intent(this,RangeFormActivity.class);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context,0,notIntent,PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Resources res = context.getResources();
+        Notification.Builder builder = new Notification.Builder(context);
+
+        builder.setContentIntent(contentIntent)
+                .setSmallIcon(R.drawable.smallnotif)
+                .setLargeIcon(BitmapFactory.decodeResource(res,R.mipmap.ic_launcher))
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentTitle(res.getString(R.string.warning))
+                .setContentText(res.getString(R.string.check))
+                .setDefaults(Notification.DEFAULT_ALL);
+
+        Notification notification = builder.build();
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFY_ID,notification);
     }
 }
