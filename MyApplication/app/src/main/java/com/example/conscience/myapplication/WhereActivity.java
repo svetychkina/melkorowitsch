8900d761d0cbacbf954e2eba51651ff462ff0966
svetychkina <svetych494@gmail.com>
2017-06-10 03:15:38 +0300

    исправление блокера

--- MyApplication/app/src/main/java/com/example/conscience/myapplication/WhereActivity.java
+++ MyApplication/app/src/main/java/com/example/conscience/myapplication/WhereActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class WhereActivity extends AppCompatActivity {
+
+public class WhereActivity extends AppCompatActivity  {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +20,20 @@ public class WhereActivity extends AppCompatActivity {
         Intent intent = new Intent(WhereActivity.this, BachelorActivity.class);
         startActivity(intent);
     }
+
+    public void onClickMag(View view) {
+        Intent intent = new Intent(WhereActivity.this, magistrActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickAsp(View view) {
+        Intent intent = new Intent(WhereActivity.this, aspActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickExtr(View view) {
+        Intent intent = new Intent(WhereActivity.this, extramActivity.class);
+        startActivity(intent);
+    }
+
 }
